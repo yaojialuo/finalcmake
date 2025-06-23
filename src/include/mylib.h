5819f8986a3e3984f7fc2b899b29879e563d5813
Galuo <yaojialuox@qq.com>
2025-06-23 17:21:01 +0800

    add src

---
+++ src/include/mylib.h
@@ -0,0 +1,7 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+// Declarations of module functions
+int add(int x, int y);
+int mul(int x, int y);
+#endif // MYLIB_H
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "mylib.h"
+
+int main() {
+    std::cout << "Hello, CMake add is:" <<add(1,2) <<std::endl;
+    return 0;
+}
